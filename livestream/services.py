15949b8fcfa2e56648da551c66a94e67be0a706b
Hruaia <hruaitluangasailo20@gmail.com>
2026-03-02 04:36:43 +0530

    hoho

--- livestream/services.py
+++ livestream/services.py
@@ -1,26 +1,24 @@
 from datetime import timedelta
-from django.conf import settings
 from livekit.api import AccessToken, VideoGrants
+from django.conf import settings
 
 
 def generate_livekit_token(user, session, is_teacher=False):
     token = AccessToken(
         settings.LIVEKIT_API_KEY,
         settings.LIVEKIT_API_SECRET,
+        identity=str(user.id),
+        name=user.email,
+        ttl=timedelta(minutes=10),   # 👈 IMPORTANT
     )
-
-    token.with_identity(str(user.id))
-    token.with_name(user.email)
 
     grants = VideoGrants(
         room_join=True,
         room=session.room_name,
         can_publish=is_teacher,
         can_subscribe=True,
-        can_publish_data=True,
     )
 
     token.with_grants(grants)
-    token.with_ttl(timedelta(hours=6))
 
     return token.to_jwt()
